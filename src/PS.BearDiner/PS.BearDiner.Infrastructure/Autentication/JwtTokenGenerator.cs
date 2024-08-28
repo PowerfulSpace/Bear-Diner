4aebd97e8d1401b424f790ef1a176809cdf6b3b8
Alexander <sany199574@gmail.com>
2024-08-29 02:53:46 +0300

    реализована инфраструктура  - JwtTokenGenerator

--- src/PS.BearDiner/PS.BearDiner.Infrastructure/Autentication/JwtTokenGenerator.cs
+++ src/PS.BearDiner/PS.BearDiner.Infrastructure/Autentication/JwtTokenGenerator.cs
@@ -1,4 +1,8 @@
-﻿using PS.BearDiner.Application.Common.Interfaces;
+﻿using Microsoft.IdentityModel.Tokens;
+using PS.BearDiner.Application.Common.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace PS.BearDiner.Infrastructure.Autentication
 {
@@ -6,7 +10,27 @@ namespace PS.BearDiner.Infrastructure.Autentication
     {
         public string GenerateToken(Guid id, string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super-secret-key")),
+                SecurityAlgorithms.HmacSha256
+                );
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,id.ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName,firstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName,lastName)
+            };
+
+            
+            var securityToken = new JwtSecurityToken(
+                issuer: "BearDiner",
+                expires: DateTime.Now.AddDays(1),
+                claims: claims,
+                signingCredentials: signingCredentials);
+
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
         }
     }
 }
--- src/PS.BearDiner/PS.BearDiner.Infrastructure/DependencyInjection.cs
+++ src/PS.BearDiner/PS.BearDiner.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PS.BearDiner.Application.Common.Interfaces;
+using PS.BearDiner.Infrastructure.Autentication;
 
 
 namespace PS.BearDiner.Infrastructure
@@ -7,6 +9,8 @@ namespace PS.BearDiner.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
             return services;
         }
     }
