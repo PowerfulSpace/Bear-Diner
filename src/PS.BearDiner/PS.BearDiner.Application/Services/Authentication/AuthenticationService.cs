47715287dff881e4156fcc412cecb02423fc5b67
Alexander <sany199574@gmail.com>
2024-09-01 01:18:49 +0300

    Изменил слой апликации на регистрацию

--- src/PS.BearDiner/PS.BearDiner.Application/Services/Authentication/AuthenticationService.cs
+++ src/PS.BearDiner/PS.BearDiner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,17 +1,37 @@
 ﻿using PS.BearDiner.Application.Common.Interfaces.Authentication;
+using PS.BearDiner.Application.Common.Interfaces.Persistence;
+using PS.BearDiner.Domain.Entities;
 
 namespace PS.BearDiner.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
-        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        private readonly IUserRepository _userRepository;
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
         {
             _jwtTokenGenerator = jwtTokenGenerator;
+            _userRepository = userRepository;
         }
 
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
+
+            if (_userRepository.GetUserByEmai(email) is not null)
+            {
+                throw new Exception("User with given email already exists.");
+            }
+
+            User user = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Password = password
+            };
+
+
+
             Guid userID = Guid.NewGuid();
 
             var token = _jwtTokenGenerator.GenerateToken(userID, firstName, lastName);
