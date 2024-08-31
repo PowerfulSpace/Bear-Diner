bfce51a825652b585b4e6961fe5920e3450eeea0
Alexander <sany199574@gmail.com>
2024-08-31 06:07:49 +0300

    Добавлен интерфейс  - IUserRepository

---
+++ src/PS.BearDiner/PS.BearDiner.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using PS.BearDiner.Domain.Entities;
+
+namespace PS.BearDiner.Application.Common.Interfaces.Persistence
+{
+    public interface IUserRepository
+    {
+        User? GetUserByEmai(string email);
+        void AddUser(User user);
+    }
+}
