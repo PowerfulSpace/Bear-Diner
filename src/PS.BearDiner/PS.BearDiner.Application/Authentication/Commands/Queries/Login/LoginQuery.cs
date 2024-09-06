3ae32742bd41d5c61f55aea64839b25444370d80
Alexander <sany199574@gmail.com>
2024-09-06 13:43:07 +0300

    Добавлен обработчик для логирования

---
+++ src/PS.BearDiner/PS.BearDiner.Application/Authentication/Commands/Queries/Login/LoginQuery.cs
@@ -0,0 +1,11 @@
+﻿using ErrorOr;
+using MediatR;
+using PS.BearDiner.Application.Services.Authentication.Common;
+
+namespace PS.BearDiner.Application.Authentication.Commands.Queries.Login
+{
+    public record LoginQuery(
+        string Email,
+        string Password
+        ) : IRequest<ErrorOr<AuthenticationResult>>;
+}
---
+++ src/PS.BearDiner/PS.BearDiner.Application/Authentication/Commands/Queries/Login/LoginQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using ErrorOr;
+using MediatR;
+using PS.BearDiner.Application.Common.Interfaces.Authentication;
+using PS.BearDiner.Application.Common.Interfaces.Persistence;
+using PS.BearDiner.Application.Services.Authentication.Common;
+using PS.BearDiner.Domain.Entities;
+using PS.BearDiner.Domain.Common.Errors;
+
+namespace PS.BearDiner.Application.Authentication.Commands.Queries.Login
+{
+    public class LoginQueryHandler : IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
+    {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IUserRepository _userRepository;
+
+        public LoginQueryHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+            _userRepository = userRepository;
+        }
+
+        public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery query, CancellationToken cancellationToken)
+        {
+            if (_userRepository.GetUserByEmai(query.Email) is not User user)
+            {
+                return Errors.Authentication.InvalidCredentials;
+            }
+
+            if (user.Password != query.Password)
+            {
+                return Errors.Authentication.InvalidCredentials;
+            }
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthenticationResult(
+               user,
+               token
+               );
+        }
+    }
+}
