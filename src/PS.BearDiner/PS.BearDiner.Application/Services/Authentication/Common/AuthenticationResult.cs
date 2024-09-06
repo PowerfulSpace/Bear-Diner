82776f34c2a8973aee39362d75ccdc9c3bfe2236
Alexander <sany199574@gmail.com>
2024-09-06 13:44:04 +0300

    Удалена вся директива сервисов

--- src/PS.BearDiner/PS.BearDiner.Application/Services/Authentication/Common/AuthenticationResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using PS.BearDiner.Domain.Entities;
-
-namespace PS.BearDiner.Application.Services.Authentication.Common
-{
-    public record AuthenticationResult(
-        User User,
-        string Token
-        );
-}
