163a4d4087b33ad7ab065aa474d98f61a2724e34
Alexander <sany199574@gmail.com>
2024-09-08 12:16:33 +0300

    Добавлен клас  - AuthenticationMappingConfig

---
+++ src/PS.BearDiner/PS.BearDiner.Api/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using Mapster;
+using PS.BearDiner.Application.Authentication.Common;
+using PS.BearDiner.Contracts.Authentication;
+
+namespace PS.BearDiner.Api.Mapping
+{
+    public class AuthenticationMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<AuthenticationResult, AuthenticationResponse>()
+                .Map(dest => dest.Token, src => src.Token)
+                .Map(dest => dest, src => src.User);
+        }
+    }
+}
