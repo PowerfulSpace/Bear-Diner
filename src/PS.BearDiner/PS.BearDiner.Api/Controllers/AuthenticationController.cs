1f546a2555ab0529a4557a74901d4b09fb0cde59
Alexander <sany199574@gmail.com>
2024-09-08 11:36:57 +0300

    Реализовал мапинг, в аутентификации. Удалил ненужный код

--- src/PS.BearDiner/PS.BearDiner.Api/Controllers/AuthenticationController.cs
+++ src/PS.BearDiner/PS.BearDiner.Api/Controllers/AuthenticationController.cs
@@ -5,6 +5,7 @@ using PS.BearDiner.Application.Authentication.Commands.Register;
 using PS.BearDiner.Application.Authentication.Queries.Login;
 using PS.BearDiner.Application.Authentication.Common;
 using PS.BearDiner.Contracts.Authentication;
+using MapsterMapper;
 
 namespace PS.BearDiner.Api.Controllers
 {
@@ -12,35 +13,36 @@ namespace PS.BearDiner.Api.Controllers
     public class AuthenticationController : ApiController
     {
         private readonly ISender _mediator;
+        private readonly IMapper _mapper;
 
-        public AuthenticationController(ISender mediator)
+        public AuthenticationController(ISender mediator, IMapper mapper)
         {
             _mediator = mediator;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterRequest request)
         {
-            var command = new RegisterCommand(
-                request.FirstName,
-                request.LastName,
-                request.Email,
-                request.Password);
+            var command = _mapper.Map<RegisterCommand>(request);
 
             ErrorOr<AuthenticationResult> authResult = await _mediator.Send(command);
 
+            //return authResult.Match(
+            //    authResult => Ok(MapAuthResult(authResult)),
+            //    errors => Problem(errors)
+            //    );
+
             return authResult.Match(
-                authResult => Ok(MapAuthResult(authResult)),
-                errors => Problem(errors)
-                );
+              authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
+              errors => Problem(errors)
+              );
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginRequest request)
         {
-            var query = new LoginQuery(
-               request.Email,
-               request.Password);
+            var query = _mapper.Map<LoginQuery>(request);
 
             ErrorOr<AuthenticationResult> authResult = await _mediator.Send(query);
 
@@ -52,19 +54,9 @@ namespace PS.BearDiner.Api.Controllers
             //}
 
             return authResult.Match(
-               authResult => Ok(MapAuthResult(authResult)),
+               authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
                errors => Problem(errors)
                );
-        }
-
-        private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
-        {
-            return new AuthenticationResponse(
-                            authResult.User.Id,
-                            authResult.User.FirstName,
-                            authResult.User.LastName,
-                            authResult.User.Email,
-                            authResult.Token);
         }
     }
 }
