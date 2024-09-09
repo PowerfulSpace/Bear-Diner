807c46596d425fc858f938a9a073983b31f7d6f1
Alexander <sany199574@gmail.com>
2024-09-09 13:40:44 +0300

    Добавлен класс для обработки запросов по пайплайну ValidationRegisterCommandBehavior

---
+++ src/PS.BearDiner/PS.BearDiner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,20 @@
+﻿using ErrorOr;
+using MediatR;
+using PS.BearDiner.Application.Authentication.Commands.Register;
+using PS.BearDiner.Application.Authentication.Common;
+
+namespace PS.BearDiner.Application.Common.Behaviors
+{
+    public class ValidationRegisterCommandBehavior : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
+    {
+        public async Task<ErrorOr<AuthenticationResult>> Handle(
+            RegisterCommand request,
+            RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
+            CancellationToken cancellationToken)
+        {
+            var result = await next();
+
+            return result;
+        }
+    }
+}
