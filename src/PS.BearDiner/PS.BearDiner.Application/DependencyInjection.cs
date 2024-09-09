19a7ec3be5b572e0fa7192cb09ec5ad8891fbcd2
Alexander <sany199574@gmail.com>
2024-09-09 14:41:46 +0300

    Подключил  - ValidationRegisterCommandBehavior

--- src/PS.BearDiner/PS.BearDiner.Application/DependencyInjection.cs
+++ src/PS.BearDiner/PS.BearDiner.Application/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ErrorOr;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using PS.BearDiner.Application.Authentication.Commands.Register;
+using PS.BearDiner.Application.Authentication.Common;
+using PS.BearDiner.Application.Common.Behaviors;
 
 namespace PS.BearDiner.Application
 {
@@ -7,6 +12,10 @@ namespace PS.BearDiner.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(x => x.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+            services.AddScoped<
+                IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>,
+                ValidationRegisterCommandBehavior>();
 
             return services;
         }
