29c310d6a5486ac166b6251a8b6b86fc11d0cabf
Alexander <sany199574@gmail.com>
2024-09-10 15:24:44 +0300

    Изменён  - ApiController

--- src/PS.BearDiner/PS.BearDiner.Application/Common/Behaviors/ValidationBehavior.cs
+++ src/PS.BearDiner/PS.BearDiner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -11,7 +11,7 @@ namespace PS.BearDiner.Application.Common.Behaviors
 
         private readonly IValidator<TRequest>? _validator;
 
-        public ValidationBehavior(IValidator<TRequest>? validator)
+        public ValidationBehavior(IValidator<TRequest>? validator = null)
         {
             _validator = validator;
         }
