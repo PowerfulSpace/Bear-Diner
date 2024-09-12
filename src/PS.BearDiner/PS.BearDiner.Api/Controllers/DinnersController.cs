b549ec34ec2a9a60fd2d913aaa9f9fbe6cff3dfc
Alexander <sany199574@gmail.com>
2024-09-12 15:37:41 +0300

    Добавлен контроллер  - DinnersController

---
+++ src/PS.BearDiner/PS.BearDiner.Api/Controllers/DinnersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.BearDiner.Api.Controllers
+{
+    public class DinnersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
