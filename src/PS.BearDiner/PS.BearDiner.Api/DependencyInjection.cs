6d42c81d735aa74850ab30f1f8a486d7ba6a3b5b
Alexander <sany199574@gmail.com>
2024-09-14 17:32:19 +0300

    Вытянул основные настройки Swager в специальный метод  - AddSwagerSetting

--- src/PS.BearDiner/PS.BearDiner.Api/DependencyInjection.cs
+++ src/PS.BearDiner/PS.BearDiner.Api/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.OpenApi.Models;
 using PS.BearDiner.Api.Common.Errors;
 using PS.BearDiner.Api.Mapping;
 
@@ -14,8 +15,43 @@ namespace PS.BearDiner.Api
 
             services.AddMappings();
 
+            AddSwagerSetting(services);
+
+            return services;
+        }
+
+        public static IServiceCollection AddSwagerSetting(this IServiceCollection services)
+        {
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                // Описание схемы авторизации
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Введите JWT токен в формате: Bearer {токен}"
+                });
+
+                // Указываем, что для каждого запроса требуется авторизация
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
 
             return services;
         }
