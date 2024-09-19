b5e8eb6bcb8d38bb43b221b93c65115895dbd792
Alexander <sany199574@gmail.com>
2024-09-19 04:19:29 +0300

    Реализован обьект  - ValueObject

--- src/PS.BearDiner/PS.BearDiner.Domain/Common/Models/ValueObject.cs
+++ src/PS.BearDiner/PS.BearDiner.Domain/Common/Models/ValueObject.cs
@@ -3,5 +3,36 @@
     public abstract class ValueObject
     {
         public abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is null || obj.GetType != GetType)
+            {
+                return false;
+            }
+
+            var valueObject = (ValueObject)obj;
+
+            var result = GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+                .Select(x => x?.GetHashCode() ?? 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+
+        public static bool operator ==(ValueObject left, ValueObject right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueObject left, ValueObject right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
