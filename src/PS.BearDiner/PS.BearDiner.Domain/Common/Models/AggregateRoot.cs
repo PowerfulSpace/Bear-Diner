09a956745a50325fed93eed632e77dc4e8d10841
Alexander <sany199574@gmail.com>
2024-09-20 06:39:25 +0300

    Реализован класс  - AggregateRoot

--- src/PS.BearDiner/PS.BearDiner.Domain/Common/Models/AggregateRoot.cs
+++ src/PS.BearDiner/PS.BearDiner.Domain/Common/Models/AggregateRoot.cs
@@ -3,6 +3,8 @@
     public abstract class AggregateRoot<TId> : Entity<TId>
         where TId : notnull
     {
-       
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
     }
 }
