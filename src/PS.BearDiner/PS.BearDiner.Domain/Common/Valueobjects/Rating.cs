900e42ec7d55e81dbb0c4ad8fff65583eff94995
Alexander <sany199574@gmail.com>
2024-09-21 03:50:43 +0300

    Добавлены классы - AverageRating - Rating

---
+++ src/PS.BearDiner/PS.BearDiner.Domain/Common/Valueobjects/Rating.cs
@@ -0,0 +1,24 @@
+﻿using PS.BearDiner.Domain.Common.Models;
+
+namespace PS.BearDiner.Domain.Common.Valueobjects
+{
+    public sealed class Rating : ValueObject
+    {
+        public int Value { get; private set; }
+
+        private Rating(int value)
+        {
+            Value = value;
+        }
+
+        public static Rating CreateNew(int rating)
+        {
+            return new Rating(rating);
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
