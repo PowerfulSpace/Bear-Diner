9c85907c68fa5521276632959ff611e8be843e25
Alexander <sany199574@gmail.com>
2024-09-24 03:30:59 +0300

    Реализован класс  - BillId

--- src/PS.BearDiner/PS.BearDiner.Domain/Bill/ValueObjects/BillId.cs
+++ src/PS.BearDiner/PS.BearDiner.Domain/Bill/ValueObjects/BillId.cs
@@ -1,6 +1,29 @@
-﻿namespace PS.BearDiner.Domain.Bill.ValueObjects
+﻿using PS.BearDiner.Domain.Common.Models;
+
+namespace PS.BearDiner.Domain.Bill.ValueObjects
 {
-    public sealed class BillId
+    public sealed class BillId : AggregateRootId<Guid>
     {
+        public override Guid Value { get; protected set; }
+
+        private BillId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static BillId Create(Guid value)
+        {
+            return new BillId(value);
+        }
+
+        public static BillId CreateUnique()
+        {
+            return new BillId(Guid.NewGuid());
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
     }
 }
