8a0ed6b76db0310afb9d9bda38155e50ce05426b
Alexander <sany199574@gmail.com>
2024-09-25 11:21:17 +0300

    Добавлен контроллер  - MenuController

---
+++ src/PS.BearDiner/PS.BearDiner.Api/Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PS.BearDiner.Contracts.Menus;
+
+namespace PS.BearDiner.Api.Controllers
+{
+    [Route("hosts/{hostId}/menu")]
+    public class MenuController : ApiController
+    {
+        public IActionResult CreateMenu(CreateMenuRequest request, string hostId)
+        {
+            return Ok();
+        }
+    }
+}
