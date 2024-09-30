cc0e58391614772f519ddc36065649e82e927901
Alexander <sany199574@gmail.com>
2024-09-30 11:41:56 +0300

    реализована полностью конфигурация для работы с таблицами

--- src/PS.BearDiner/PS.BearDiner.Infrastructure/Persistence/Configurations/MenuConfigurations.cs
+++ src/PS.BearDiner/PS.BearDiner.Infrastructure/Persistence/Configurations/MenuConfigurations.cs
@@ -12,6 +12,8 @@ namespace PS.BearDiner.Infrastructure.Persistence.Configurations
         {
             ConfigureMenusTable(builder);
             ConfigureMenuSectionsTable(builder);
+            ConfigureMenuDinnerIdsTable(builder);
+            ConfigureMenuReviewIdsTable(builder);
         }
 
         private void ConfigureMenusTable(EntityTypeBuilder<Menu> builder)
@@ -62,8 +64,76 @@ namespace PS.BearDiner.Infrastructure.Persistence.Configurations
 
                 sb.Property(m => m.Description)
                     .HasMaxLength(100);
+
+
+                sb.OwnsMany(s => s.Items, ib =>
+                {
+                    ib.ToTable("MenuItems");
+
+                    ib.WithOwner().HasForeignKey("MenuSectionId", "MenuId");
+
+                    ib.HasKey(nameof(MenuItemId),"MenuSectionId", "MenuId");
+
+                    ib.Property(i => i.Id)
+                        .HasColumnName("MenuItemId")
+                        .ValueGeneratedNever()
+                        .HasConversion(
+                            id => id.Value,
+                            value => MenuItemId.Create(value));
+
+                    ib.Property(m => m.Name)
+                        .HasMaxLength(100);
+
+                    ib.Property(m => m.Description)
+                        .HasMaxLength(100);
+                });
+
+                sb.Navigation(s => s.Items).Metadata.SetField("_items");
+                sb.Navigation(s => s.Items).UsePropertyAccessMode(PropertyAccessMode.Field);
+
             });
 
+
+            builder.Metadata.FindNavigation(nameof(Menu.Sections))!
+                .SetPropertyAccessMode(PropertyAccessMode.Field);
+        }
+
+        private void ConfigureMenuDinnerIdsTable(EntityTypeBuilder<Menu> builder)
+        {
+            builder.OwnsMany(m => m.DinnerIds, dib =>
+            {
+                dib.ToTable("MenuDinnerIds");
+
+                dib.WithOwner().HasForeignKey("MenuId");
+
+                dib.HasKey("Id");
+
+                dib.Property(d => d.Value)
+                    .HasColumnName("DinnerId")
+                .ValueGeneratedNever();
+            });
+
+            builder.Metadata.FindNavigation(nameof(Menu.DinnerIds))!
+                .SetPropertyAccessMode(PropertyAccessMode.Field);
+        }
+
+        private void ConfigureMenuReviewIdsTable(EntityTypeBuilder<Menu> builder)
+        {
+            builder.OwnsMany(m => m.MenuReviewIds, dib =>
+            {
+                dib.ToTable("MenuReviewIds");
+
+                dib.WithOwner().HasForeignKey("MenuId");
+
+                dib.HasKey("Id");
+
+                dib.Property(d => d.Value)
+                    .HasColumnName("ReviewId")
+                .ValueGeneratedNever();
+            });
+
+            builder.Metadata.FindNavigation(nameof(Menu.MenuReviewIds))!
+                .SetPropertyAccessMode(PropertyAccessMode.Field);
         }
 
     }
