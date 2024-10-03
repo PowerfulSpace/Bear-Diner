42a40937550fc5ffc3bd28512d2b6dc9391c5898
Alexander <sany199574@gmail.com>
2024-10-03 13:47:23 +0300

    Добавлена конфигцрация  - GuestConfigurations

---
+++ src/PS.BearDiner/PS.BearDiner.Infrastructure/Persistence/Configurations/GuestConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PS.BearDiner.Domain.GuestAggregate.ValueObjects;
+using PS.BearDiner.Domain.Guests;
+
+namespace PS.BearDiner.Infrastructure.Persistence.Configurations
+{
+    public sealed class GuestConfigurations : IEntityTypeConfiguration<Guest>
+    {
+        public void Configure(EntityTypeBuilder<Guest> builder)
+        {
+            ConfigureGuestsTable(builder);
+        }
+
+        private void ConfigureGuestsTable(EntityTypeBuilder<Guest> builder)
+        {
+            builder
+                .ToTable("Guests");
+
+            builder
+                .HasKey(g => g.Id);
+
+            builder
+                .Property(g => g.Id)
+                .ValueGeneratedNever()
+                .HasConversion(
+                    id => id.Value,
+                    value => GuestId.Create(value));
+        }
+    }
+}
