f0a417e0021d0ff3e2ffa885dbd76a553c0a98fe
Alexander <sany199574@gmail.com>
2024-10-03 13:22:11 +0300

    Изменена конфигцрация для меода  - ConfigureMenusTable

--- src/PS.BearDiner/PS.BearDiner.Infrastructure/Persistence/Configurations/MenuConfigurations.cs
+++ src/PS.BearDiner/PS.BearDiner.Infrastructure/Persistence/Configurations/MenuConfigurations.cs
@@ -22,21 +22,25 @@ namespace PS.BearDiner.Infrastructure.Persistence.Configurations
 
             builder.HasKey(m => m.Id);
 
-            builder.Property(m => m.Id)
+            builder
+                .Property(m => m.Id)
                 .ValueGeneratedNever()
                 .HasConversion(
                     id => id.Value,
                     value => MenuId.Create(value));
 
-            builder.Property(m => m.Name)
+            builder
+                .Property(m => m.Name)
                 .HasMaxLength(100);
 
-            builder.Property(m => m.Description)
+            builder
+                .Property(m => m.Description)
                 .HasMaxLength(100);
 
             builder.OwnsOne(m => m.AverageRating);
 
-            builder.Property(m => m.HostId)
+            builder
+                .Property(m => m.HostId)
                 .HasConversion(
                     id => id.Value,
                     value => HostId.Create(value));
