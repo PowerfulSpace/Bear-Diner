b90b81d86dc6263fd5ad0ae2a7faf413f6174d05
Alexander <sany199574@gmail.com>
2024-10-04 14:54:55 +0300

    Добавлены директории Acceptance Tests End To End Tests Functional Tests Integration Tests Unit Tests
    для  UnitTests добавлены тесты
    
    PS.BearDiner.Api.UnitTests
    PS.BearDiner.Application.UnitTests
    PS.BearDiner.Contracts.UnitTests
    PS.BearDiner.Domain.UnitTests
    PS.BearDiner.Infrastructure.UnitTests

---
+++ tests/PS.BearDiner.Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PS.BearDiner.Api.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/PS.BearDiner.Contracts.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PS.BearDiner.Contracts.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
