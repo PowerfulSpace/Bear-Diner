dbfc15d9073a2752d38be3fc4522611b94e93ed7
Alexander <sany199574@gmail.com>
2024-10-06 17:16:37 +0300

    реализован класс для реализации утилит для меня команды  - CreateMenuCommandUtils

--- tests/PS.BearDiner.Application.UnitTests/Menus/Commands/TestUtils/CreateMenuCommandUtils.cs
+++ tests/PS.BearDiner.Application.UnitTests/Menus/Commands/TestUtils/CreateMenuCommandUtils.cs
@@ -1,6 +1,31 @@
-﻿namespace PS.BearDiner.Application.UnitTests.Menus.Commands.TestUtils
+﻿
+using PS.BearDiner.Application.Menus.Commands.CreateMenu;
+using PS.BearDiner.Application.UnitTests.TestUtils.Constants;
+
+namespace PS.BearDiner.Application.UnitTests.Menus.Commands.TestUtils
 {
     public class CreateMenuCommandUtils
     {
+        public static CreateMenuCommand CreateCommand() =>
+            new CreateMenuCommand(
+                Guid.Parse(Constants.Host.Id.ToString()!),
+                Constants.Menu.Name,
+                Constants.Menu.Description,
+                CreateSectionsCommand());
+
+        public static List<CreateMenuSectionCommand> CreateSectionsCommand(int sectionCount = 1) =>
+            Enumerable.Range(0, sectionCount)
+                .Select(index => new CreateMenuSectionCommand(
+                    Constants.Menu.SectionNameFromIndex(index),
+                    Constants.Menu.SectionDescriptionFromIndex(index),
+                    CreateItemsCommand()))
+                .ToList();
+
+        public static List<CreateMenuItemCommand> CreateItemsCommand(int itemCount = 1) =>
+            Enumerable.Range(0, itemCount)
+                .Select(index => new CreateMenuItemCommand(
+                    Constants.Menu.ItemNameFromIndex(index),
+                    Constants.Menu.ItemDescriptionFromIndex(index)))
+                .ToList();
     }
 }
---
+++ tests/PS.BearDiner.Application.UnitTests/TestUtils/Constants/Constants.Host.cs
@@ -0,0 +1,12 @@
+﻿using PS.BearDiner.Domain.Hosts.ValueObjects;
+
+namespace PS.BearDiner.Application.UnitTests.TestUtils.Constants
+{
+    public static partial class Constants
+    {
+        public static class Host
+        {
+            public static readonly HostId Id = HostId.Create(Guid.Parse("856A4CE2-F0C6-4DE9-A5FC-0546E33E33A2"));
+        }
+    }
+}
