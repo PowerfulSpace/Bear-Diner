039c6c27912dd4f59625b5f805a21e700f846b2f
Alexander <sany199574@gmail.com>
2024-10-06 18:28:22 +0300

    Исправлена ошибка в  - CreateMenuCommandHandlerTests

--- tests/PS.BearDiner.Application.UnitTests/Menus/Commands/CreateMenu/CreateMenuCommandHandlerTests.cs
+++ tests/PS.BearDiner.Application.UnitTests/Menus/Commands/CreateMenu/CreateMenuCommandHandlerTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using PS.BearDiner.Application.Common.Interfaces.Persistence;
 using PS.BearDiner.Application.Menus.Commands.CreateMenu;
 using PS.BearDiner.Application.UnitTests.Menus.Commands.TestUtils;
+using PS.BearDiner.Application.UnitTests.TestUtils.Menus.Extensions;
 
 namespace PS.BearDiner.Application.UnitTests.Menus.Commands.CreateMenu
 {
@@ -14,7 +15,7 @@ namespace PS.BearDiner.Application.UnitTests.Menus.Commands.CreateMenu
         public CreateMenuCommandHandlerTests()
         {
             _mockMenuRepository = new Mock<IMenuRepository>();
-            _handler = new CreateMenuCommandHandler(_mockMenuRepository);
+            _handler = new CreateMenuCommandHandler(_mockMenuRepository.Object);
         }
 
         public async Task CreateMenuCommandHandler_WhenMenuIsValid_ShouldCreateAndReturnMenu()
@@ -23,12 +24,16 @@ namespace PS.BearDiner.Application.UnitTests.Menus.Commands.CreateMenu
             var CreateMenuCommand = CreateMenuCommandUtils.CreateCommand();
 
             //Act
+            //Invoke the handler
             var result = await _handler.Handle(CreateMenuCommand, default);
 
             //Assert
             result.IsError.Should().BeFalse();
-
             result.Value.ValidateCreatedFrom(CreateMenuCommand);
+            _mockMenuRepository.Verify(m => m.Add(result.Value), Times.Once);
+
+            // 1. Validate correct menu created based on command
+            // 2. Menu added to repository
         }
     }
 }
