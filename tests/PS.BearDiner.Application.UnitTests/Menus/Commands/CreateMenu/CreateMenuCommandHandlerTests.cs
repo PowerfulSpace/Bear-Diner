798be27cc4367b6cc51821dd662087574b8bb473
Alexander <sany199574@gmail.com>
2024-10-06 18:06:55 +0300

    Добавлен класс  - MenuExtensions

--- tests/PS.BearDiner.Application.UnitTests/Menus/Commands/CreateMenu/CreateMenuCommandHandlerTests.cs
+++ tests/PS.BearDiner.Application.UnitTests/Menus/Commands/CreateMenu/CreateMenuCommandHandlerTests.cs
@@ -1,10 +1,34 @@
-﻿namespace PS.BearDiner.Application.UnitTests.Menus.Commands.CreateMenu
+﻿using FluentAssertions;
+using Moq;
+using PS.BearDiner.Application.Common.Interfaces.Persistence;
+using PS.BearDiner.Application.Menus.Commands.CreateMenu;
+using PS.BearDiner.Application.UnitTests.Menus.Commands.TestUtils;
+
+namespace PS.BearDiner.Application.UnitTests.Menus.Commands.CreateMenu
 {
     public class CreateMenuCommandHandlerTests
     {
-        public void CreateMenuCommandHandler_WhenMenuIsValid_ShouldCreateAndReturnMenu()
-        {
+        private readonly CreateMenuCommandHandler _handler;
+        private readonly Mock<IMenuRepository> _mockMenuRepository;
 
+        public CreateMenuCommandHandlerTests()
+        {
+            _mockMenuRepository = new Mock<IMenuRepository>();
+            _handler = new CreateMenuCommandHandler(_mockMenuRepository);
+        }
+
+        public async Task CreateMenuCommandHandler_WhenMenuIsValid_ShouldCreateAndReturnMenu()
+        {
+            //Arrange
+            var CreateMenuCommand = CreateMenuCommandUtils.CreateCommand();
+
+            //Act
+            var result = await _handler.Handle(CreateMenuCommand, default);
+
+            //Assert
+            result.IsError.Should().BeFalse();
+
+            result.Value.ValidateCreatedFrom(CreateMenuCommand);
         }
     }
 }
---
+++ tests/PS.BearDiner.Application.UnitTests/TestUtils/Menus/Extensions/MenuExtensions.Validations.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using PS.BearDiner.Application.Menus.Commands.CreateMenu;
+using PS.BearDiner.Domain.Menus;
+using PS.BearDiner.Domain.Menus.Entities;
+
+namespace PS.BearDiner.Application.UnitTests.TestUtils.Menus.Extensions
+{
+    public static partial class MenuExtensions
+    {
+        public static void ValidateCreatedFrom(this Menu menu, CreateMenuCommand command)
+        {
+            menu.Name.Should().Be(command.Name);
+            menu.Description.Should().Be(command.Description);
+            menu.Sections.Should().HaveSameCount(command.Sections);
+            menu.Sections.Zip(command.Sections).ToList().ForEach(pair => ValidateSection(pair.First, pair.Second));
+        }
+
+        public static void ValidateSection(MenuSection section, CreateMenuSectionCommand command)
+        {
+            section.Id.Should().NotBeNull();
+            section.Name.Should().Be(command.Name);
+            section.Description.Should().Be(command.Description);
+            section.Items.Should().HaveSameCount(command.Items);
+            section.Items.Zip(command.Items).ToList().ForEach(pair => ValidateItem(pair.First, pair.Second));
+        }
+
+        public static void ValidateItem(MenuItem item, CreateMenuItemCommand command)
+        {
+            item.Id.Should().NotBeNull();
+            item.Name.Should().Be(command.Name);
+            item.Description.Should().Be(command.Description);
+        }
+    }
+}
